6bae01a2123e7ea871f7f5e7320a25535807959f
PROJUKTIWINDOWS\PROJUKTI WINDOWS 10 <info2programmer@gmail.com>
2018-06-05 18:48:54 +0530

    JS file update

--- www/js/home.js
+++ www/js/home.js
@@ -1,54 +1,19 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
+    
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    
     onDeviceReady: function() {
-        // app.receivedEvent('deviceready');
-        // app.openGellery();
-        // app.openCamera();
-        $('#lblLocality').html(localStorage.getItem('area'));
+        // Set Default Text And Value
+        $('#lblLocality').html(localStorage.getItem('area')); //Display Area Here
     },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        // var parentElement = document.getElementById(id);
-        // var listeningElement = parentElement.querySelector('.listening');
-        // var receivedElement = parentElement.querySelector('.received');
-
-        // listeningElement.setAttribute('style', 'display:none;');
-        // receivedElement.setAttribute('style', 'display:block;');
-
-        // console.log('Received Event: ' + id);
-    },
+    
+    // Open Camera Using this section
     openCamera : function(){
         navigator.camera.getPicture(onSuccess, onFail, {
             quality: 70,
@@ -84,6 +49,8 @@ var app = {
             window.plugins.toast.showLongBottom('Failed because: ' + message);
         }
     },
+
+    // Open Gallery Uisng this section
     openGellery : function() {
         navigator.camera.getPicture(onSuccess, onFail, {
             quality: 70,
@@ -120,6 +87,7 @@ var app = {
     }
     
 };
+
 function swipperminus(value) {
     let curretn_value = $('#' + value).html();
     if (curretn_value!=0)
@@ -130,20 +98,14 @@ function swipperminus(value) {
         curretn_value =0;
     }
     $('#' + value).html(curretn_value);
-    // console.log(curretn_value);
 
 }
 
 function swipperadd(value) {
     let curretn_value = $('#' + value).html();
     curretn_value = parseInt(curretn_value)+1;
-    // if (curretn_value != 0) {
-    //     curretn_value = curretn_value - 1;
-    // }
-    // else {
-    //     curretn_value = 0;
-    // }
+   
     $('#' + value).html(curretn_value);
-    // console.log(curretn_value);
+    
 }
 
