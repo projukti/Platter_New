c45ae9772f76d45d268e5780fd0e7304fffe70e7
Saikat Bhadury <info2programmer@gmail.com>
2018-06-06 17:21:55 +0530

    restaurent list impliment successfully

--- www/js/home.js
+++ www/js/home.js
@@ -13,6 +13,8 @@ var app = {
         // Set Default Text And Value
         $('#lblLocality').html(localStorage.getItem('area')); //Display Area Here
         app.topFiveRestaurants();
+        app.topTwelveRestaurants();
+        app.generalRestaurants();
     },
 
     // Back Button Off 
@@ -110,6 +112,142 @@ var app = {
             }
             
         }).fail();
+    },
+
+    // This Function For Top 12 Restaurents
+    topTwelveRestaurants : function(){
+        let topTwelveRestaurantData = '' 
+        $.ajax({
+            type: "POST",
+            url: "http://platterexoticfood.com/pladmin/manage_api/restaurant_top_twelve_list/",
+            dataType: "JSON"
+        }).done(function (rply) {
+            console.log(rply);
+            for (list in rply.restaurant) {
+                topTwelveRestaurantData += '<li>'
+                if (app.isRestaurantOpen(rply.restaurant[list].opening_time, rply.restaurant[list].closing_time) == "open")
+                {
+                    topTwelveRestaurantData += '<a href="/restaurent-details/' + rply.restaurant[list].restaurant_id + '" class="item-content" style="color:#676767;">'
+                    topTwelveRestaurantData += '<div class="item-media">'
+                    topTwelveRestaurantData += '<img src="http://platterexoticfood.com/pladmin/uploads/restaurant/' + rply.restaurant[list].restaurant_image + '" width="80" style="border-radius: 50%;height: 85px;width: 85px;"/>'
+                }
+                else{
+                    topTwelveRestaurantData += '<a href="javascript:void(0);" class="item-content" style="color:#676767;">'
+                    topTwelveRestaurantData += '<div class="item-media">'
+                    topTwelveRestaurantData += '<img src="http://platterexoticfood.com/pladmin/uploads/restaurant/' + rply.restaurant[list].restaurant_image + '" width="80" style="border-radius: 50%;height: 85px;width: 85px;-webkit-filter: grayscale(100%); filter: grayscale(100%);"/>'
+                }
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '<div class="item-inner">'
+                topTwelveRestaurantData += '<div class="item-title-row">'
+                topTwelveRestaurantData += '<div class="item-title" style="font-size: 14px; font-weight: bold;">' + rply.restaurant[list].restaurant_name + '</div>'
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '<div class="item-subtitle" style="font-size: 12px;">' + rply.restaurant[list].cuisine_tags + '</div>'
+                topTwelveRestaurantData += '<hr style="height: 0px; color: #fff;">'
+                topTwelveRestaurantData += '<div class="item-subtitle">'
+                topTwelveRestaurantData += '<div class="row">'
+                topTwelveRestaurantData += '<div class="col-40" style="font-size: 12px;">'
+                topTwelveRestaurantData += '<i class="icon material-icons md-only" style="font-size: 18px;margin-right: -5px;">star</i>' + rply.restaurant[list].avg_rating
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '<div class="col-60" style="font-size: 12px;text-align: right;">'
+                topTwelveRestaurantData += '<img src="img/iconset/rupee.png" style="height: 14px;margin-bottom: -3px;">'
+                topTwelveRestaurantData += '<span>' + rply.restaurant[list].two_person_cost +' FOR TWO</span>'
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '</div>'
+                topTwelveRestaurantData += '</a>'
+                topTwelveRestaurantData += '</li>'
+                $('#lblTopTwelveRestaurents').html(topTwelveRestaurantData)
+            }
+
+        }).fail();
+    },
+
+    // Rest Of The Rasturants List
+    generalRestaurants: function () {
+        let generalRestaurents = ''
+        $.ajax({
+            type: "POST",
+            url: "http://platterexoticfood.com/pladmin/manage_api/restaurant_general_list/",
+            dataType: "JSON"
+        }).done(function (rply) {
+            console.log(rply);
+            for (list in rply.restaurant) {
+                generalRestaurents += '<li>'
+                if (app.isRestaurantOpen(rply.restaurant[list].opening_time, rply.restaurant[list].closing_time) == "open") {
+                    generalRestaurents += '<a href="/restaurent-details/' + rply.restaurant[list].restaurant_id + '" class="item-content" style="color:#676767;">'
+                    generalRestaurents += '<div class="item-media">'
+                    generalRestaurents += '<img src="http://platterexoticfood.com/pladmin/uploads/restaurant/' + rply.restaurant[list].restaurant_image + '" width="80" style="border-radius: 50%;height: 85px;width: 85px;"/>'
+                }
+                else {
+                    generalRestaurents += '<a href="javascript:void(0);" onclick="' + app.custToastMessage("Rasturant will open at : " + rply.restaurant[list].opening_time) +'" class="item-content" style="color:#676767;">'
+                    generalRestaurents += '<div class="item-media">'
+                    generalRestaurents += '<img src="http://platterexoticfood.com/pladmin/uploads/restaurant/' + rply.restaurant[list].restaurant_image + '" width="80" style="border-radius: 50%;height: 85px;width: 85px;-webkit-filter: grayscale(100%); filter: grayscale(100%);"/>'
+                }
+                generalRestaurents += '</div>'
+                generalRestaurents += '<div class="item-inner">'
+                generalRestaurents += '<div class="item-title-row">'
+                generalRestaurents += '<div class="item-title" style="font-size: 14px; font-weight: bold;">' + rply.restaurant[list].restaurant_name + '</div>'
+                generalRestaurents += '</div>'
+                generalRestaurents += '<div class="item-subtitle" style="font-size: 12px;">' + rply.restaurant[list].cuisine_tags + '</div>'
+                generalRestaurents += '<hr style="height: 0px; color: #fff;">'
+                generalRestaurents += '<div class="item-subtitle">'
+                generalRestaurents += '<div class="row">'
+                generalRestaurents += '<div class="col-40" style="font-size: 12px;">'
+                generalRestaurents += '<i class="icon material-icons md-only" style="font-size: 18px;margin-right: -5px;">star</i>' + rply.restaurant[list].avg_rating
+                generalRestaurents += '</div>'
+                generalRestaurents += '<div class="col-60" style="font-size: 12px;text-align: right;">'
+                generalRestaurents += '<img src="img/iconset/rupee.png" style="height: 14px;margin-bottom: -3px;">'
+                generalRestaurents += '<span>' + rply.restaurant[list].two_person_cost + ' FOR TWO</span>'
+                generalRestaurents += '</div>'
+                generalRestaurents += '</div>'
+                generalRestaurents += '</div>'
+                generalRestaurents += '</div>'
+                generalRestaurents += '</a>'
+                generalRestaurents += '</li>'
+                $('#lblGeneralRestaurents').html(generalRestaurents)
+            }
+
+        }).fail();
+    },
+
+    // This Function Check Restaurent is opent or closed
+    isRestaurantOpen: function (opening, closing) {
+        opening = opening.split(':');
+        closing = closing.split(':');
+        openingH = parseInt(opening[0]);
+        openingM = parseInt(opening[1]);
+        closingH = parseInt(closing[0]);
+        closingM = parseInt(closing[1]);
+        var d = new Date(); // current time
+        var H = d.getHours();
+        var M = d.getMinutes();
+        is_open = false;
+        if (openingH < closingH) {
+            if (H > openingH && H < closingH) {
+                is_open = true
+            } else if (H === openingH && M >= openingM) {
+                is_open = true
+            } else if (H === closingH && M <= closingM) {
+                is_open = true
+            }
+        } else if (openingH > closingH) {
+            if (H > openingH || H < closingH) {
+                is_open = true
+            } else if (H === openingH && M >= openingH) {
+                is_open = true
+            } else if (H === closingH && M <= closingH) {
+                is_open = true
+            }
+        } else {
+            is_open = true
+        }
+        return is_open ? 'open' : 'closed';
+    },
+
+    // This Function For Custome Toast Message
+    custToastMessage : function(message){
+        window.plugins.toast.showLongBottom(message);
     },
     
 };
