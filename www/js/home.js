a7550a5f3fbc0155d47ff8e551bcaf2227fdbada
Saikat Bhadury <info2programmer@gmail.com>
2018-06-12 13:09:11 +0530

    map testing

--- www/js/home.js
+++ www/js/home.js
@@ -654,80 +654,80 @@ function swipperMinusMenuDetails(menu_id, restaurant_id) {
 
 }
 
-// Get geo coordinates
+// // Get geo coordinates
 
-function getMapLocation() {
+// function getMapLocation() {
 
-    navigator.geolocation.getCurrentPosition
-        (onMapSuccess, onMapError, { enableHighAccuracy: true });
-}
+//     navigator.geolocation.getCurrentPosition
+//         (onMapSuccess, onMapError, { enableHighAccuracy: true });
+// }
 
-// Success callback for get geo coordinates
+// // Success callback for get geo coordinates
 
-var onMapSuccess = function (position) {
+// var onMapSuccess = function (position) {
 
-    Latitude = position.coords.latitude;
-    Longitude = position.coords.longitude;
+//     Latitude = position.coords.latitude;
+//     Longitude = position.coords.longitude;
 
-    getMap(Latitude, Longitude);
+//     getMap(Latitude, Longitude);
 
-}
+// }
 
-// Get map by using coordinates
+// // Get map by using coordinates
 
-function getMap(latitude, longitude) {
+// function getMap(latitude, longitude) {
 
-    var mapOptions = {
-        center: new google.maps.LatLng(0, 0),
-        zoom: 1,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+//     var mapOptions = {
+//         center: new google.maps.LatLng(0, 0),
+//         zoom: 1,
+//         mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
 
-    map = new google.maps.Map
-        (document.getElementById("map"), mapOptions);
+//     map = new google.maps.Map
+//         (document.getElementById("map"), mapOptions);
 
 
-    var latLong = new google.maps.LatLng(latitude, longitude);
+//     var latLong = new google.maps.LatLng(latitude, longitude);
 
-    var marker = new google.maps.Marker({
-        position: latLong
-    });
+//     var marker = new google.maps.Marker({
+//         position: latLong
+//     });
 
-    marker.setMap(map);
-    map.setZoom(15);
-    map.setCenter(marker.getPosition());
-}
+//     marker.setMap(map);
+//     map.setZoom(15);
+//     map.setCenter(marker.getPosition());
+// }
 
-// Success callback for watching your changing position
+// // Success callback for watching your changing position
 
-var onMapWatchSuccess = function (position) {
+// var onMapWatchSuccess = function (position) {
 
-    var updatedLatitude = position.coords.latitude;
-    var updatedLongitude = position.coords.longitude;
+//     var updatedLatitude = position.coords.latitude;
+//     var updatedLongitude = position.coords.longitude;
 
-    if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
+//     if (updatedLatitude != Latitude && updatedLongitude != Longitude) {
 
-        Latitude = updatedLatitude;
-        Longitude = updatedLongitude;
+//         Latitude = updatedLatitude;
+//         Longitude = updatedLongitude;
 
-        getMap(updatedLatitude, updatedLongitude);
-    }
-}
+//         getMap(updatedLatitude, updatedLongitude);
+//     }
+// }
 
-// Error callback
+// // Error callback
 
-function onMapError(error) {
-    console.log('code: ' + error.code + '\n' +
-        'message: ' + error.message + '\n');
-}
+// function onMapError(error) {
+//     console.log('code: ' + error.code + '\n' +
+//         'message: ' + error.message + '\n');
+// }
 
-// Watch your changing position
+// // Watch your changing position
 
-function watchMapPosition() {
+// function watchMapPosition() {
 
-    return navigator.geolocation.watchPosition
-        (onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
-}
+//     return navigator.geolocation.watchPosition
+//         (onMapWatchSuccess, onMapError, { enableHighAccuracy: true });
+// }
 
 
 
