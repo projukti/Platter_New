d6b2389fdb7590054463205b719f030e409223b8
Saikat Bhadury <info2programmer@gmail.com>
2018-06-18 11:30:50 +0530

    Back button debuging

--- www/js/home.js
+++ www/js/home.js
@@ -21,8 +21,8 @@ var app = {
     },
 
     // Back Button Off 
-    onBackKeyDown: function () {
-        homeView.router.back();
+    onBackKeyDown: function (viewName) {
+        viewName.router.back();
     },
 
     // Open Camera Using this section
