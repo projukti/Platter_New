65cb3cf4f5e68e13252b301f242e819fb3a7bff9
Saikat Bhadury <info2programmer@gmail.com>
2018-06-25 17:23:06 +0530

    Track Debuging

--- www/js/home.js
+++ www/js/home.js
@@ -875,7 +875,109 @@ var app = {
                 $('#trackingMap').hide();
             }
             else{
-                this.trackOrder();
+                let customerLocation = {
+                    lat: parseFloat(localStorage.getItem('lat')),
+                    lng: parseFloat(localStorage.getItem('lang'))
+                };
+                var map = new google.maps.Map(document.getElementById('trackingMap'), {
+                    center: customerLocation,
+                    zoom: 12,
+                    mapTypeControl: false,
+                    disableDefaultUI: false,
+                    styles: [{
+                        elementType: 'geometry',
+                        stylers: [{ color: '#f5f5f5' }]
+                    },
+                    {
+                        elementType: 'labels.icon',
+                        stylers: [{ visibility: 'on' }]
+                    },
+                    {
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#616161' }]
+                    },
+                    {
+                        elementType: 'labels.text.stroke',
+                        stylers: [{ color: '#9a9a9a' }]
+                    },
+                    {
+                        featureType: 'administrative.land_parcel',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#bdbdbd' }]
+                    },
+                    {
+                        featureType: 'poi',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#9a9a9a' }]
+                    },
+                    {
+                        featureType: 'poi',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#757575' }]
+                    },
+                    {
+                        featureType: 'poi.park',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#e5e5e5' }]
+                    },
+                    {
+                        featureType: 'poi.park',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#9e9e9e' }]
+                    },
+                    {
+                        featureType: 'road',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#ffffff' }]
+                    },
+                    {
+                        featureType: 'road.arterial',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#757575' }]
+                    },
+                    {
+                        featureType: 'road.highway',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#dadada' }]
+                    },
+                    {
+                        featureType: 'road.highway',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#616161' }]
+                    },
+                    {
+                        featureType: 'road.local',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#9e9e9e' }]
+                    },
+                    {
+                        featureType: 'transit.line',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#e5e5e5' }]
+                    },
+                    {
+                        featureType: 'transit.station',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#eeeeee' }]
+                    },
+                    {
+                        featureType: 'water',
+                        elementType: 'geometry',
+                        stylers: [{ color: '#c9c9c9' }]
+                    },
+                    {
+                        featureType: 'water',
+                        elementType: 'labels.text.fill',
+                        stylers: [{ color: '#9e9e9e' }]
+                    }
+                    ]
+                });
+                var directionsDisplay = new google.maps.DirectionsRenderer({
+                    map: map,
+                    suppressMarkers: false
+                });
+                var directionsService = new google.maps.DirectionsService();
+                app.trackOrder();
             }
         });
         // console.log(customerLocation)
