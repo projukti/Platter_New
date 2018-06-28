67f9d8da720a0d8e12475cd5307abeb956ebe66f
Saikat Bhadury <info2programmer@gmail.com>
2018-06-28 12:14:41 +0530

    Search bar change

--- www/js/app.js
+++ www/js/app.js
@@ -73,10 +73,80 @@ var searchbar = app.searchbar.create({
   searchIn: '.item-title',
   on: {
     search(sb, query, previousQuery) {
-      console.log(query, previousQuery);
+      let searchValue=''
+      $.ajax({
+        type: "post",
+        url: serverUrl + "search_dish/",
+        //data: { dishname: searchQuery,resttype:'all', cuisine: 'all', lat: localStorage.getItem('lat'), lang: localStorage.getItem('lang'),user:user, pincode:  localStorage.getItem('curr_pin')},
+        data: { dishname: query, resttype: 'all', cuisine: 'all', lat: '22.4724863', lang: '88.3785082', user: user, pincode: '700047' },
+        dataType: "JSON"
+      }).done(function (rply) {
+        console.log(rply)
+        for (list in rply.menu_data) {
+          searchValue += '<li>'
+          if (app.isRestaurantOpen(rply.menu_data[list].opening_time, rply.menu_data[list].closing_time) == "open") {
+            searchValue += '<a href="/restaurent-details/' + rply.menu_data[list].restaurant_id + '/' + rply.menu_data[list].restaurant_name + '/" class="item-content" style="color:#676767;">'
+            searchValue += '<div class="item-media">'
+            searchValue += '<img src="http://platterexoticfood.com/pladmin/uploads/restaurant/' + rply.menu_data[list].restaurant_image + '" width="80" style="border-radius: 50%;height: 85px;width: 85px;"/>'
+          }
+          else {
+            let cloaseMessage = "Restaurest will open at : " + rply.menu_data[list].opening_time
+            searchValue += '<a href="javascript:void(0);" onclick="javascript:app.custToastMessage(\'' + cloaseMessage + '\')" class="item-content" style="color:#676767;">'
+            searchValue += '<div class="item-media">'
+            searchValue += '<img src="http://platterexoticfood.com/pladmin/uploads/restaurant/' + rply.menu_data[list].restaurant_image + '" width="80" style="border-radius: 50%;height: 85px;width: 85px;-webkit-filter: grayscale(100%); filter: grayscale(100%);"/>'
+          }
+          searchValue += '</div>'
+          searchValue += '<div class="item-inner">'
+          searchValue += '<div class="item-title-row">'
+          searchValue += '<div class="item-title" style="font-size: 14px; font-weight: bold;">' + rply.menu_data[list].restaurant_name + '</div>'
+          searchValue += '</div>'
+          searchValue += '<div class="item-subtitle" style="font-size: 12px;">' + rply.menu_data[list].cuisine_tags + '</div>'
+          searchValue += '<hr style="height: 0px; color: #fff;">'
+          searchValue += '<div class="item-subtitle">'
+          searchValue += '<div class="row">'
+          searchValue += '<div class="col-40" style="font-size: 12px;">'
+          searchValue += '<i class="icon material-icons md-only" style="font-size: 18px;margin-right: -5px;">star</i>' + rply.menu_data[list].avg_rating
+          searchValue += '</div>'
+          searchValue += '<div class="col-60" style="font-size: 12px;text-align: right;">'
+          searchValue += '<img src="img/iconset/rupee.png" style="height: 14px;margin-bottom: -3px;">'
+          searchValue += '<span>' + rply.menu_data[list].two_person_cost + ' FOR TWO</span>'
+          searchValue += '</div>'
+          searchValue += '</div>'
+          searchValue += '</div>'
+          searchValue += '</div>'
+          searchValue += '</a>'
+          searchValue += '</li>'
+
+          $('#normalListing').hide();
+          $('#lblFilterRestaurents').html(searchValue)
+          $('#lblFilterRestaurentCount').html("Match Restaurents");
+          $('#filterResult').show();
+
+          // if (searchQuery.length === 0) {
+          //   $('#lblRestaurentSearchList').html('')
+          // }
+          // else {
+          //   $('#lblRestaurentSearchList').html(searchValue)
+          // }
+
+        }
+      });
+      
+      if(query==""){
+        $('#filterResult').hide();
+        $('#lblFilterRestaurents').html('')
+        $('#lblFilterRestaurentCount').html("");
+        $('#normalListing').show();
+      }
     },
     enable: function () {
       console.log('Searchbar enabled')
+    },
+    disable: function(){
+      $('#filterResult').hide();
+      $('#lblFilterRestaurents').html('')
+      $('#lblFilterRestaurentCount').html("");
+      $('#normalListing').show();
     }
   }
 });
